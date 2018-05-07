970f692cefb8a907933427d0b17a884f3cce220d
joaopsalgado <salgadojoaopedro@hotmail.com>
2018-05-07 19:58:50 -0300

    atualizando classe java/ pacote

--- Exercicios/TerceiroExercicio.java
+++ Exercicios/TerceiroExercicio.java
@@ -1,10 +1,10 @@
-package exerciciodeintegracao;
+package com.github.joaopsalgado.integracao.ex3;
 
 import java.io.BufferedReader;
 import java.io.FileReader;
 import java.io.IOException;
 
-public class TerceiroExercicio {
+public class ex3 {
 
     public static void main(String[] args) throws IOException {
 
