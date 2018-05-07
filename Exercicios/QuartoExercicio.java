617f365c8c5857f3df88b77a1e505aab485ed43d
joaopsalgado <salgadojoaopedro@hotmail.com>
2018-05-07 19:59:57 -0300

    Atualizacao da classe java / pacote

--- Exercicios/QuartoExercicio.java
+++ Exercicios/QuartoExercicio.java
@@ -1,12 +1,12 @@
-package exerciciodeintegracao;
+package com.github.joaopsalgado.ex4;
 
 import java.io.FileNotFoundException;
 import java.io.IOException;
 import java.io.PrintWriter;
 import java.io.UnsupportedEncodingException;
 
-public class QuartoExercicio {
-       
+public class ex4 {
+   
     public static void main(String[] args) throws UnsupportedEncodingException, FileNotFoundException, IOException {
        PrintWriter pw = new PrintWriter(args[0],"UTF-8");
             pw.write(args[1]);
