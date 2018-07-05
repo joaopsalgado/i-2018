11ceef79df553e97941955fb709a8585370d124c
joaopsalgado <salgadojoaopedro@hotmail.com>
2018-07-04 22:51:35 -0300

    Create Class calcados.java

---
+++ ExericicioTopico3/Ex2/Sapatos.java
@@ -0,0 +1,34 @@
+package br.ufg.inf.es.integracao;
+//Criacao da classe calcados
+public class Sapatos extends Calcados {
+
+    private int red;
+    private int green;
+    private int blue;
+ 
+//Getters and Setters 
+    public int getRed() {
+        return red;
+    }
+
+    public void setRed(int red) {
+        this.red = red;
+    }
+
+    public int getGreen() {
+        return green;
+    }
+    
+    public void setGreen(int green) {
+        this.green = green;
+    }
+
+    public int getBlue() {
+        return blue;
+    }
+  
+    public void setBlue(int blue) {
+        this.blue = blue;
+    }
+
+}
