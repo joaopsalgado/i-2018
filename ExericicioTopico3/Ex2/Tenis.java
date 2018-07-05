61a98a50616c4d1f8282abbaefed04c87257ceec
joaopsalgado <salgadojoaopedro@hotmail.com>
2018-07-04 22:53:19 -0300

    Create class tenis

---
+++ ExericicioTopico3/Ex2/Tenis.java
@@ -0,0 +1,21 @@
+package br.ufg.inf.es.integracao;
+
+import java.util.Collection;
+import java.util.Iterator;
+import java.util.List;
+import java.util.ListIterator;
+
+//Criacao da classe Tenis
+public class Tenis extends Calcados {
+
+    private String esporte;
+    
+    public String getEsporte() {
+        return esporte;
+    }
+
+    public void setEsporte(String esporte) {
+        this.esporte = esporte;
+    }
+
+}
